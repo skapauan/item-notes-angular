9ddc3aa033ccda5a6fddcd674387cd134ff1b132
skapauan <skapauan@users.noreply.github.com>
2021-07-02 17:37:49 -0500

    Format with ESLint and Prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { AppRoutingModule } from './app-routing.module'
+import { IntroComponent } from './main/intro/intro.component'
+import { LoadingComponent } from './main/loading/loading.component'
+import { MainComponent } from './main/main/main.component'
+import { NavigationComponent } from './main/navigation/navigation.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
