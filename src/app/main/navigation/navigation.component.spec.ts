f957f63f5a0c0b87bbce9fa93d04c81d057c534f
skapauan <skapauan@users.noreply.github.com>
2021-07-05 23:22:00 -0500

    Navigation

--- src/app/main/navigation/navigation.component.spec.ts
+++ src/app/main/navigation/navigation.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
 
-import { NavigationComponent } from './navigation.component'
+import { NavigationComponent, navigationLinks } from './navigation.component'
+
+describe('navigationLinks', () => {
+    it('should contain unique paths', () => {
+        const paths = navigationLinks.map(({ path }) => path)
+        for (let i = paths.length - 1; i >= 0; i--) {
+            expect(paths.indexOf(paths[i])).toEqual(i)
+        }
+    })
+
+    it('should contain unique titles', () => {
+        const titles = navigationLinks.map(({ title }) => title)
+        for (let i = titles.length - 1; i >= 0; i--) {
+            expect(titles.indexOf(titles[i])).toEqual(i)
+        }
+    })
+})
 
 describe('NavigationComponent', () => {
     let component: NavigationComponent
@@ -8,6 +25,7 @@ describe('NavigationComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             declarations: [NavigationComponent],
         }).compileComponents()
     })
@@ -20,5 +38,17 @@ describe('NavigationComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy()
+    })
+
+    it('should render the navigation links', () => {
+        const compiled = fixture.nativeElement
+        const elements = compiled.querySelectorAll('nav ul li a')
+        for (let i = navigationLinks.length - 1; i >= 0; i--) {
+            const a = elements.item(i)
+            const n = navigationLinks[i]
+            expect(a).toBeTruthy()
+            expect(a.getAttribute('href')).toContain(n.path)
+            expect(a.textContent).toContain(n.title)
+        }
     })
 })
--- src/app/main/navigation/navigation.component.ts
+++ src/app/main/navigation/navigation.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core'
 import { Strings } from 'src/strings/strings'
 
+export const navigationLinks = [
+    { path: '', title: Strings.ScreenNameView },
+    { path: 'fields', title: Strings.ScreenNameFields },
+    { path: 'save', title: Strings.ScreenNameSave },
+    { path: 'open', title: Strings.ScreenNameOpen },
+    { path: 'settings', title: Strings.ScreenNameSettings },
+]
+
 @Component({
     selector: 'app-navigation',
     templateUrl: './navigation.component.html',
     styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-    links = [
-        { path: '', title: Strings.ScreenNameView },
-        { path: 'fields', title: Strings.ScreenNameFields },
-        { path: 'save', title: Strings.ScreenNameSave },
-        { path: 'open', title: Strings.ScreenNameOpen },
-        { path: 'settings', title: Strings.ScreenNameSettings },
-    ]
+    links = navigationLinks
 
     constructor() {
         return
